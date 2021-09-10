d4ade68a6745b8cf3d2bf716a95ed87920900085
Koumori97 <d.e.fledermaus86@gmail.com>
2021-09-10 14:33:57 +0200

    09092021

--- javascript/home.js
+++ javascript/home.js
@@ -1,20 +1,20 @@
-let cart = [];
-console.log("hew", window.localStorage.getItem("username"))
+// let cart = [];
+// console.log("hew", window.localStorage.getItem("username"))
 
 
-// Adding to cart
+// // Adding to cart
 
-function addToCart(index){
-    cart.push(index);
-    let add_btn = document.querySelector(`.AtCbtn-${index}`);
-    add_btn.style.display = "none";
-    alert("Comic moved to cart, You're lucky we had one in stock.");
-    populateCart();
-}
+// function addToCart(index){
+//     cart.push(index);
+//     let add_btn = document.querySelector(`.AtCbtn-${index}`);
+//     add_btn.style.display = "none";
+//     alert("Comic moved to cart, You're lucky we had one in stock.");
+//     populateCart();
+// }
 
-function viewCart(){
-    document.querySelector(".cart").classList.toggle("active");
-}
+// function viewCart(){
+//     document.querySelector(".cart").classList.toggle("active");
+// }
 
 
 // Fetching products from database and then displaying them
@@ -39,7 +39,7 @@ function createLimitedCards() {
                 </div>
                 <div class="rest">
                     <h4>R ${comic[3]}.00</h4>
-                    <button onclick="addToCart(${comic[0]})" class="AtCbtn-${comic[0]}">Add to cart</button>
+                    <button onclick="viewComic(${comic[0]})" class="viewbtn-${comic[0]}">View</button>
                 </div>
            </div>`;
                 console.log(index);
@@ -72,7 +72,7 @@ function createBestCards() {
                 </div>
                 <div class="rest">
                     <h4>R ${comic[3]}.00</h4>
-                    <button onclick="addToCart(${comic[0]})" class="AtCbtn-${comic[0]}">Add to cart</button>
+                    <button onclick="viewComic(${comic[0]})" class="viewbtn-${comic[0]}">View</button>
                 </div>
            </div>`;
                 console.log(index);
@@ -84,3 +84,32 @@ function createBestCards() {
 }
 
 createBestCards();
+
+// modal
+
+function viewComic(index) {
+    console.log(index)
+    let comic_div = document.querySelector(".mod-content");
+    comic_div.classList.toggle("show")
+    comic_div.innerHTML = ``;
+    actModal()
+
+    if(index !== -1){
+        fetch("https://safe-stream-00881.herokuapp.com/view-all/")
+        .then((request) => request.json())
+        .then((obj) => {
+            data = obj.data;
+            data.forEach((comic) => {
+                if(comic[0] == index) {
+                    comic_div.innerHTML += `
+                    <button onclick="viewComic(-1)">X</button>
+                    <h1 class="title">${comic[1]}</h1>
+                    <h2 class="universe">${comic[4]}</h2>
+                    <h4 class="era">${comic[5]}</h4>
+                    <p class="description">${comic[2]}</p>
+                    <h4 class="price">R${comic[3]}</h4>`;
+                }
+            });
+        });
+    }
+}
--- javascript/modal.js
+++ javascript/modal.js
@@ -1,4 +1,3 @@
 function actModal() {
     document.getElementById("modal").classList.toggle("modal-active");
-    document.getElementById("blur").classList.toggle("active");
 }
